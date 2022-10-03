8d57ce4e86d22057cdc3a47e1048f14da76e91fc
Benjamin Milčić <benjamin.milcic@gmail.com>
2022-10-03 10:50:52 +0200

    v0.0051

--- src/app/home/multiple-choice/multiple-choice.component.ts
+++ src/app/home/multiple-choice/multiple-choice.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
-import { IonSegment, SegmentCustomEvent } from '@ionic/angular';
+import { IonSegment } from '@ionic/angular';
 import { Subscription } from 'rxjs';
 import { DatabaseService } from 'src/app/shared/database.service';
 import { MyVocable } from 'src/app/shared/models';
