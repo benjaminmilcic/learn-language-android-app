d8b689b32b6c797943aab5594be1e501ab45056b
Benjamin Milcic <benjamin.milcic@gmail.com>
2023-06-24 00:37:49 +0200

    code refactoring

--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -7,15 +7,13 @@ import { HomePage } from './home.page';
 import { HomePageRoutingModule } from './home-routing.module';
 import { OptionsComponent } from '../../components/options/options.component';
 import { PracticeComponent } from '../../components/practice/practice.component';
-import { EditWordlistsComponent } from 'src/app/components/edit-wordlists/edit-wordlists.component';
 
 @NgModule({
   imports: [CommonModule, FormsModule, IonicModule, HomePageRoutingModule],
   declarations: [
     HomePage,
     OptionsComponent,
-    PracticeComponent,
-    EditWordlistsComponent,
+    PracticeComponent
   ],
 })
 export class HomePageModule {}
